fb907a4b62e9df7cacb3ec0d3035075ec16eca83
UniqueNL <samtaal@outlook.com>
2016-10-16 16:07:03 +0200

    Added seeds and reset database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+song1 = Song.create( {name: "Thriller" })
+song2 = Song.create( {name: "Billie Jean" })
+song3 = Song.create( {name: "Black and White" })
+song4 = Song.create( {name: "Elevation" })
+song5 = Song.create( {name: "One" })
+song6 = Song.create( {name: "Beautiful Day" })
+
+artist1 = Artist.create ( {name: "Michael Jackson" })
+artist2 = Artist.create ( {name: "U2" })
+
+artist1.songs << [song1]
+artist1.songs << [song2]
+artist1.songs << [song3]
+artist1.save
+
+artist2.songs << [song4]
+artist2.songs << [song5]
+artist2.songs << [song6]
+artist2.save
+
