3e0b398aeafcfcdc8f89646fd5fdd8315b7314f4
Sam Taal <Sam@Sams-Macbook.local>
2016-10-23 22:47:37 +0200

    Try to include AJAX

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,9 +8,35 @@ class ArtistsController < ApplicationController
     else
       @artists = Artist.all
     end
+
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
+
 
   def show
     @artist = Artist.find(params[:id])
+    @songs = @artist.songs
   end
+
+  def new
+  @song = Song.new
+  end
+
+  def create
+    @song  = Song.create(song_params)
+  end
+
+  private
+
+    def all_tasks
+      @songs = Song.all
+    end
+
+    def task_params
+      params.require(:song).permit(:name, :artist_id)
+    end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,4 @@
 class Song < ApplicationRecord
   belongs_to :artist
+  validates_presence_of :name
 end
